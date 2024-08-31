88c65ef5b285153cd4582a20c1fffee9b4f6631e
KiwiScreams <screamskiwi3@gmail.com>
2024-08-31 18:27:05 +0400

    change language

--- src/pages/SvgDetail.tsx
+++ src/pages/SvgDetail.tsx
@@ -8,6 +8,10 @@ const SvgDetail = () => {
   const { id } = useParams();
   const { components } = useSvgComponents();
   const navigate = useNavigate();
+  const [language, setLanguage] = useState("js");
+  const handleLanguageChange = (lang: string) => {
+    setLanguage(lang);
+  };
   if (!components) {
     return <div>Loading...</div>;
   }
@@ -37,9 +41,26 @@ const SvgDetail = () => {
           )}
         </DetailContainer>
         <SvgSource>
+          <ButtonContainer>
+            <button onClick={() => handleLanguageChange("default")}>
+              Default
+            </button>
+            <button onClick={() => handleLanguageChange("js")}>JS</button>
+            <button onClick={() => handleLanguageChange("ts")}>TS</button>
+          </ButtonContainer>
           {components.length > 0 && (
-            <SyntaxHighlighter language="typescript" style={atomDark}>
-              {components[0]["js-snippet"]}
+            // <SyntaxHighlighter language="typescript" style={atomDark}>
+            //   {components[0]["js-snippet"]}
+            // </SyntaxHighlighter>
+            <SyntaxHighlighter
+              language={language === "ts" ? "typescript" : "javascript"}
+              style={atomDark}
+            >
+              {language === "default"
+                ? components[0].base
+                : language === "js"
+                ? components[0]["js-snippet"]
+                : components[0]["ts-snippet"]}
             </SyntaxHighlighter>
           )}
         </SvgSource>
@@ -65,21 +86,68 @@ const DetailSection = styled.div`
     font-size: 22px;
     text-align: center;
   }
+  @media (max-width: 830px) {
+    flex-direction: column;
+    gap: 30px;
+  }
 `;
 const DetailContainer = styled.div`
   border: 1px solid #2765d8;
   padding: 24px;
   border-radius: 20px;
-  max-height: 80vh;
+  width: 40%;
+  @media (max-width: 1000px) {
+    padding: 10px;
+  }
+  @media (max-width: 830px) {
+    width: 80%;
+    margin: auto;
+  }
+  @media (max-width: 530px) {
+    width: 98%;
+    margin: auto;
+  }
 `;
 const DetailImage = styled.div`
-  width: 50%;
+  width: 40%;
   overflow: hidden;
   margin: auto;
-  margin-bottom: 20px;
+  margin-bottom: 50px;
 `;
 const SvgSource = styled.div`
   width: 50%;
-  height: 70vh;
-  overflow: auto;
+  height: 80vh;
+  display: flex;
+  position: relative;
+  ::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+  }
+  @media (max-width: 830px) {
+    width: 80%;
+    margin: auto;
+  }
+  @media (max-width: 530px) {
+    width: 98%;
+    margin: auto;
+  }
+`;
+const ButtonContainer = styled.button`
+  width: 100%;
+  display: flex;
+  margin: auto;
+  position: absolute;
+  top: -10px;
+  background-color: transparent;
+  button {
+    width: 33.5%;
+    padding: 5px 10px;
+    background-color: #2765d8;
+    color: white;
+    font-size: 16px;
+    &:hover {
+      background-color: #3498db;
+      border-color: #3498db;
+    }
+  }
 `;
