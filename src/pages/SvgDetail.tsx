1e5c3d0e080ab8aa5ff645d798f0cb153c820ba7
KiwiScreams <screamskiwi3@gmail.com>
2024-08-31 17:51:49 +0400

    detail style

--- src/pages/SvgDetail.tsx
+++ src/pages/SvgDetail.tsx
@@ -3,6 +3,7 @@ import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { useNavigate, useParams } from "react-router-dom";
 import useSvgComponents from "../store/useSvgComponents";
 import { useState, useEffect } from "react";
+import styled from "styled-components";
 const SvgDetail = () => {
   const { id } = useParams();
   const { components } = useSvgComponents();
@@ -19,28 +20,66 @@ const SvgDetail = () => {
   }, [currentSvg]);
   return (
     <>
-      <div>
-        {currentSvg && (
-          <div>
-            <h1>{id}</h1>
-            <h1>{currentSvg.name}</h1>
-            <img
-              src={import.meta.env.VITE_API_URL + currentSvg.image}
-              alt={currentSvg.name}
-            />
-            <p>{currentSvg.description}</p>
-          </div>
-        )}
-        <div style={{ width: "500px" }}>
+      <DetailSection>
+        <DetailContainer>
+          {currentSvg && (
+            <div>
+              <h2>{currentSvg.name}</h2>
+              <DetailImage>
+                <img
+                  style={{ width: "100%" }}
+                  src={import.meta.env.VITE_API_URL + currentSvg.image}
+                  alt={currentSvg.name}
+                />
+              </DetailImage>
+              <p>{currentSvg.description}</p>
+            </div>
+          )}
+        </DetailContainer>
+        <SvgSource>
           {components.length > 0 && (
             <SyntaxHighlighter language="typescript" style={atomDark}>
               {components[0]["js-snippet"]}
             </SyntaxHighlighter>
           )}
-        </div>
-      </div>
+        </SvgSource>
+      </DetailSection>
     </>
   );
 };
 
 export default SvgDetail;
+const DetailSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 24px;
+  h2 {
+    font-size: 40px;
+    text-align: center;
+    margin-bottom: 30px;
+    color: #2765d8;
+    font-weight: 400;
+  }
+  p {
+    font-size: 22px;
+    text-align: center;
+  }
+`;
+const DetailContainer = styled.div`
+  border: 1px solid #2765d8;
+  padding: 24px;
+  border-radius: 20px;
+  max-height: 80vh;
+`;
+const DetailImage = styled.div`
+  width: 50%;
+  overflow: hidden;
+  margin: auto;
+  margin-bottom: 20px;
+`;
+const SvgSource = styled.div`
+  width: 50%;
+  height: 70vh;
+  overflow: auto;
+`;
