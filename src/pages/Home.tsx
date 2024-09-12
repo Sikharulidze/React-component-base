52deb2fc57bfc1b81ac813070fdb4e20246e5685
KiwiScreams <screamskiwi3@gmail.com>
2024-09-12 14:04:57 +0400

    import images and icons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,15 @@
+import bg from "../assets/images/3dBackground.jpg";
+import feedback from "../assets/images/customer-experience.png";
+import icon from "../assets/images/component.png";
+import logo from "../assets/images/logo.png";
 const Home = () => {
+
   return (
-    <div>
-      <h1>Welcome to React Components Central</h1>
-      <p>
-        Explore our comprehensive collection of React components designed for
-        developers of all levels. Our site offers a range of components, each
-        with detailed code snippets in both JavaScript and TypeScript. Whether
-        you're building a new project or enhancing an existing one, you'll find
-        easy-to-use elements that fit seamlessly into your workflow. Click on
-        any component to view its code, copy it, and integrate it into your
-        applications with ease. Dive in and elevate your development experience
-        today!
-      </p>
-      <p>SVG icons components</p>
-      <h3>We Value Your Feedback</h3>
-    </div>
+    <>
+
+    </>
   );
 };
 
 export default Home;
+
