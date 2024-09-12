8de9a21a1fc18a9b32cad3ee3c340d45b463e76c
KiwiScreams <screamskiwi3@gmail.com>
2024-09-12 14:05:54 +0400

    location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,19 @@ import bg from "../assets/images/3dBackground.jpg";
 import feedback from "../assets/images/customer-experience.png";
 import icon from "../assets/images/component.png";
 import logo from "../assets/images/logo.png";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 const Home = () => {
-
-  return (
-    <>
-
-    </>
-  );
+  const location = useLocation();
+  const [isHomePage, setIsHomePage] = useState(false);
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setIsHomePage(true);
+    } else {
+      setIsHomePage(false);
+    }
+  }, [location]);
+  return <></>;
 };
 
 export default Home;
-
