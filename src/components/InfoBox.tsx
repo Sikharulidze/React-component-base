59a53731e6d2a512b72772e151390f3747741bd4
KiwiScreams <screamskiwi3@gmail.com>
2024-09-13 19:17:26 +0400

    fix styles

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -15,13 +15,13 @@ const Infobox = styled.div`
   font-size: 18px;
   font-weight: bold;
   border: 3px solid #439cfb;
-  padding: 15px;
+  padding: 6px 15px;
   border-radius: 15px;
   width: fit-content;
   color: #439cfb;
   position: fixed;
   right: 3px;
-  top: 80px;
+  top: 70px;
   transition: 0.3s;
   box-sizing: border-box;
   &:hover {
